9314e22ecc6ec2ecb782ab1d256bb39764ce5943
Noah Anderson <noahbanderson@gmail.com>
2025-12-05 10:08:35 -0600

    Day 2 Complete

--- dart/lib/day_2/main.dart
+++ dart/lib/day_2/main.dart
@@ -1,7 +1,22 @@
+import 'package:collection/collection.dart';
+
 import '../utils.dart';
 
 final input = getInput().readAsLinesSync();
 final _logger = getLogger();
+
+final Iterable<(int, int)> ranges = input[0].split(',').map((final range) {
+  final parts = range.split('-').map(int.parse).toList();
+  return (parts[0], parts[1]);
+});
+
+final expandedRanges = ranges.map(
+  (final range) => List.generate(
+      range.$2 - range.$1 + 1, //
+      (final index) => (index + range.$1).toString()),
+);
+
+final allValues = expandedRanges.expand((final i) => i);
 
 void main() {
   part1();
@@ -9,9 +24,28 @@ void main() {
 }
 
 void part1() {
-  _logger.info('Result <part_1>: ');
+  final re = RegExp(r'^(.+)\1$');
+  final List<int> invalidIds = [];
+  for (final value in allValues) {
+    if (re.hasMatch(value)) {
+      invalidIds.add(int.parse(value));
+    }
+  }
+
+  final sum = invalidIds.sum;
+
+  _logger.info('Result <part_1>: $sum');
 }
 
 void part2() {
-  _logger.info('Result <part_2>: ');
+  final re = RegExp(r'^(.+?)\1+$');
+  final List<int> invalidIds = [];
+  for (final value in allValues) {
+    if (re.hasMatch(value)) {
+      invalidIds.add(int.parse(value));
+    }
+  }
+
+  final sum = invalidIds.sum;
+  _logger.info('Result <part_2>: $sum');
 }
